310542deedfc41b109fa6795abef3440aad74112
KidusTadesseCode <Kidustadessearega@gmail.com>
2023-01-19 21:03:52 -0500

    mod react router

--- src/index.js
+++ src/index.js
@@ -6,11 +6,15 @@ import App from './App';
 import { store } from './ReduxStore/Store';
 import {Provider} from "react-redux"
 
+import { HashRouter } from "react-router-dom";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <HashRouter>
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
+  </HashRouter>
 );
